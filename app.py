0994299706aff24c14748557e2395c4f34b7d45c
Kassem Zeinab <kazeinab123@gmail.com>
2024-10-16 16:29:33 +0300

    Starting from scratch

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-from flask import Flask, render_template, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api, Resource, reqparse, abort
-from config import Config
+from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-if __name__ == ("__main__"):
+if __name__ == '__main__':
     app.run(debug=True)
