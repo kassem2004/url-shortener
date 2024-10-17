fe2e69e377d69ceaf2438da230dd68df736ad9e8
Kassem Zeinab <kazeinab123@gmail.com>
2024-10-17 09:39:25 +0300

    Integrated JS, added url mapping dictionary

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_restful import Api, Resource
 import random, string
 
@@ -12,13 +12,24 @@ def Home():
 url_mapping = {}
 
 def shorten_url():
-    return "https://gde.ly/" + ''.join(random.choices(string.ascii_letters, k = 7))
+    return "https://gde.ly/" + ''.join(random.choices(string.ascii_letters, k=7))
+
+# just for testing
+@app.route('/view_mappings')
+def view_mappings():
+    return jsonify(url_mapping)
 
 class Shorten_URL(Resource):
-    def get(self):
-        url = request.args.get('url_input')
+    def post(self): #handle post request at /url_shortener endpoint
+        data = request.get_json()  
+        url = data.get('url_input')
+
         shortened_url = shorten_url()
-        return {"original" : url, "shortened": shortened_url}
+        url_mapping[url] = shortened_url
+        return jsonify({
+            "original_url": url,
+            "shortened_url": shortened_url
+        })
 
 api.add_resource(Shorten_URL, "/url_shortener")
 
