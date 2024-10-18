e3a991669be1caabab0c6e58f5a9054a9d36ec9b
Kassem Zeinab <kazeinab123@gmail.com>
2024-10-19 00:14:05 +0300

    Integrated MySQL db

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request, jsonify, redirect
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as db
 import random, string
+import os
+
+password = os.getenv('SQL_PASS')
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:{password}@localhost:3306/url_shortener_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Urls(db.Model):
+    __tablename__ = "url_data"
+    url_ID = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    shortened_url = db.Column(db.String(100), unique=True, nullable=False)
+    click_count = db.Column(db.Integer, default=0)
 
 @app.route('/')
 def Home():
@@ -25,7 +40,11 @@ class Shorten_URL(Resource):
         url = data.get('url_input')
 
         shortened_url = shorten_url()
-        url_mapping[shortened_url] = url
+        
+        new_mapping = Urls(original_url=url, shortened_url=shortened_url)
+        db.session.add(new_mapping)
+        db.session.commit()
+
         return jsonify({
             "original_url": url,
             "shortened_url": f"https://gde.ly/{shortened_url}"
@@ -33,8 +52,8 @@ class Shorten_URL(Resource):
     
 class URL_Redirect(Resource):
     def get(self, shortened_url):
-        url = url_mapping.get(shortened_url)
-        return redirect(url)
+        url = Urls.query.filter_by(shortened_url=shortened_url).first()
+        return redirect(url.original_url)
 
 api.add_resource(Shorten_URL, "/url_shortener")
 api.add_resource(URL_Redirect, "/gde.ly/<shortened_url>")
